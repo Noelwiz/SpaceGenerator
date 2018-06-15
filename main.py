7c0a87ca4393c3b751ff77ff8de8d7137327d096
Noelwiz <Noelwiz1@gmail.com>
2018-06-14 20:56:28 -0700

    Added Main

---
+++ main.py
@@ -0,0 +1,11 @@
+#imports
+#using as so there's zero mix up with the objects defined within ever
+import Moon as MN
+import Planet as PT
+import Star as  ST
+import StarSystem as STSys
+
+
+def main():
+    print("there is no interface yet")
+    input("press enter to quit the generator")
